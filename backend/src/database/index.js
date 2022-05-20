3f2eecd38f625bbee57f214af9bb8844980d9723
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2022-05-19 23:59:14 -0300

    fix: export default structure

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,4 +1,4 @@
 import { MongoClient } from 'mongodb';
 import config from '../config';
-
-export default new MongoClient(config.mongodb_url);
+const mongo = new MongoClient(config.mongodb_url);
+export default mongo; 
